ddac07a652a926c2171d459ba2088538c81074a5
Phong Nguyen <nhphong1406@gmail.com>
2012-03-13 08:15:19 +0700

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+## MongoTodo - MONGOID + MongoDB + Rails 3.2 Application Example
+
+Setup:  
++ Install / run  MongoDB  
++ `bundle install`  
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoTodo::Application.config.secret_token = 'bda52666686df296ffb51409a346c1f777ec6d405c540abdb5c74aef44280c50515b10dcd7f104c2bd8b18d4aaa4fb69a9a026a224b8f29e7b546e7c1cf7944d'
