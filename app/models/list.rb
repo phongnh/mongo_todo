9257c44905def56777c5da45ef5bf73b8af672a9
Phong Nguyen <nhphong1406@gmail.com>
2012-03-13 12:52:13 +0700

    Add models based on MongoDB document

---
+++ app/models/list.rb
@@ -0,0 +1,19 @@
+class List
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name,        type: String
+  field :description, type: String
+
+  index :name, unique: true
+
+  embeds_many :tasks
+
+  validates :name,
+            presence: true,
+            length: { maximum: 255 },
+            uniqueness: true
+
+  attr_accessible :name, :description
+
+end
---
+++ app/models/task.rb
@@ -0,0 +1,20 @@
+class Task
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :description, type: String
+  field :completed,   type: Boolean, default: false
+  field :due,         type: Date
+  field :note,        type: String
+
+  index :due
+
+  embedded_in :list
+
+  validates :description,
+            presence: true,
+            length: { maximum: 255 }
+
+  validates :due,
+            presence: true
+end
